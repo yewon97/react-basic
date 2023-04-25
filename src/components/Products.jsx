74ef762f9c1361b6f8156a35767bea21851a4423
yewon97 <dpdnjs9936@gmail.com>
2023-04-25 23:35:06 +0900

    useEffect 의존성주입하기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Products() {
-	const [count, setCount] = useState(0);
 	const [products, setProducts] = useState([]);
+	const [checked, setChecked] = useState(false);
+	const handleChange = () => setChecked(prev => !prev);
 
 	useEffect(() => {
-		fetch('data/products.json')
+		fetch(`data/${checked ? 'sale_' : ''}products.json`)
 			.then(res => res.json())
 			.then(data => {
 				console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴!');
@@ -14,10 +15,12 @@ export default function Products() {
 		return () => {
 			console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
 		}
-	}, []);
-
+	}, [checked]);
+	
 	return (
 		<>
+			<input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
+			<label htmlFor="checkbox">Show Only 🔥 Sale</label>
 			<ul>
 				{products.map((product) => (
 					<li key={product.id}>
@@ -28,7 +31,6 @@ export default function Products() {
 					</li>
 				))}
 			</ul>
-			<button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
 		</>
 	)
 }
