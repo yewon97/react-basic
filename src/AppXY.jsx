68fb67df6894b75274f0a34cd118cc23c51d2259
yewon97 <dpdnjs9936@gmail.com>
2023-04-26 00:05:11 +0900

    포인터 이동 내가 먼저 만들기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './AppXY.css';
 
 export default function AppXY() {
-	
+	const [pointer, setPointer] = useState([0, 0]);
+
+	const handlePointer = (e) => {
+		setPointer([e.clientX, e.clientY]);
+	}
 
 	return (
-		<div className='container'>
-			<div className="pointer" />
+		<div className='container' onMouseMove={handlePointer}>
+			<div className="pointer" style={{left : pointer[0], top: pointer[1]}} />
 		</div>
 	)
 }
