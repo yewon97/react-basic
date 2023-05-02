8be95ae5ad01c08422a6af62e2d4984ae7f3ea2b
yewon97 <dpdnjs9936@gmail.com>
2023-05-02 22:00:59 +0900

    useState -> useReducer 사용해서 상태관리 하기

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,75 +1,56 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+import personReducer from './reducer/person-reducer';
 
 export default function AppMentors(props) {
-  const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-	const handleUpdate = () => {
-		const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-		const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+  const handleUpdate = () => {
+    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+    dispatch({ type: 'updated', prev, current });
+  };
 
-		setPerson(person => ({
-			...person,
-			mentors: person.mentors.map(mentor => {
-				if(mentor.name === prev) {
-					return {...mentor, name : current}
-				}
-				return mentor;
-			})
-		}))
-	}
+  const handleAdd = () => {
+    const name = prompt(`멘토의 이름을 알려주세요!`);
+    const title = prompt(`멘토의 타이틀을 알려주세요!`);
+    dispatch({ type: 'added', name, title });
+  };
 
-	const handleAdd = () => {
-		const name = prompt(`멘토의 이름을 알려주세요!`);
-		const title = prompt(`멘토의 타이틀을 알려주세요!`);
-		setPerson(person => ({
-			...person,
-			mentors: [
-				...person.mentors,
-				{name, title}
-			]
-		}))
-	}
-
-	const handleDelete = () => {
-		const name = prompt(`누구를 삭제하고 싶은가요?`);
-		setPerson(person => ({
-			...person,
-			mentors: person.mentors.filter(m => m.name !== name)
-		}))
-	}
+  const handleDelete = () => {
+    const name = prompt(`누구를 삭제하고 싶은가요?`);
+    dispatch({ type: 'deleted', name });
+  };
 
   return (
     <div>
       <h1>
         {person.name}는 {person.title}
       </h1>
-      <p>
-        {person.name}의 멘토는 :
-      </p>
-			<ul>
-				{person.mentors.map((mentor, index) => {
-					const text = `${mentor.name} (${mentor.title})`;
-					return <li key={index} children={text} />
-				})}
-			</ul>
-      <button onClick={handleUpdate}>멘토의  이름 바꾸기</button>
-			<button onClick={handleDelete}>멘토 삭제하기</button>
-			<button onClick={handleAdd}>멘토 추가하기</button>
+      <p>{person.name}의 멘토는 :</p>
+      <ul>
+        {person.mentors.map((mentor, index) => {
+          const text = `${mentor.name} (${mentor.title})`;
+          return <li key={index} children={text} />;
+        })}
+      </ul>
+      <button onClick={handleUpdate}>멘토의 이름 바꾸기</button>
+      <button onClick={handleDelete}>멘토 삭제하기</button>
+      <button onClick={handleAdd}>멘토 추가하기</button>
     </div>
   );
 }
 
 const initialPerson = {
-	name: '엘리',
-	title: '개발자',
-	mentors: [
-		{
-			name: '밥',
-			title: '시니어개발자',
-		},	
-		{
-			name: '제임스',
-			title: '시니어개발자',
-		}
-	],
-}
+  name: '엘리',
+  title: '개발자',
+  mentors: [
+    {
+      name: '밥',
+      title: '시니어개발자',
+    },
+    {
+      name: '제임스',
+      title: '시니어개발자',
+    },
+  ],
+};
