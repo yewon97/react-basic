39e343675418ba617f52fe349371de2a404d088c
yewon97 <dpdnjs9936@gmail.com>
2023-05-04 00:07:07 +0900

    useMemo, memo, useCallback을 사용해서 re-rendering 제한

--- src/AppMentorsButton.jsx
+++ src/AppMentorsButton.jsx
@@ -1,25 +1,25 @@
-import React, { useMemo, useReducer } from 'react';
+import React, { memo, useCallback, useMemo, useReducer } from 'react';
 import personReducer from './reducer/person-reducer';
 
 export default function AppMentorsButton() {
   const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
     dispatch({ type: 'updated', prev, current });
-  };
+  }, []);
 
-  const handleAdd = () => {
+  const handleAdd = useCallback(() => {
     const name = prompt(`멘토의 이름은?`);
     const title = prompt(`멘토의 직함은?`);
     dispatch({ type: 'added', name, title });
-  };
+  }, []);
 
-  const handleDelete = () => {
+  const handleDelete = useCallback(() => {
     const name = prompt(`누구를 삭제하고 싶은가요?`);
     dispatch({ type: 'deleted', name });
-  };
+  }, []);
 
   return (
     <div>
@@ -41,9 +41,14 @@ export default function AppMentorsButton() {
   );
 }
 
-function Button({ text, onClick }) {
+const Button = memo(({ text, onClick }) => {
   console.log('Button', text, 're-rendering 😜');
+
+  // 컴포넌트 내부에서 무거운일을 하거나
+  // 매번 호출되길 바라지 않는다면
+  // useMemo로 처리하면된다.
   const result = useMemo(() => calculateSomething(), []);
+
   return (
     <button
       onClick={onClick}
@@ -57,7 +62,7 @@ function Button({ text, onClick }) {
       {text}
     </button>
   );
-}
+});
 
 function calculateSomething() {
   for (let i = 0; i < 10000; i++) {
